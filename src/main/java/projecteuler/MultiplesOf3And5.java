0a5eeac18f314cec7a4bd195ae8bb8147fbe5826
Pantelis Zoupis <p.zoupis@pamediakopes.gr>
2019-04-05 10:48:54 +0300

    Solve problem 1 for project euler

--- src/main/java/projecteuler/MultiplesOf3And5.java
+++ src/main/java/projecteuler/MultiplesOf3And5.java
@@ -1,4 +1,21 @@
 package projecteuler;
 
+/**
+ * Problem link: https://projecteuler.net/problem=1
+ */
 public class MultiplesOf3And5 {
+    public static int multipleOf3And5(int limit) {
+        int sum = 0;
+
+        for (int i = 0; i < limit; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multipleOf3And5(1000));
+    }
 }
